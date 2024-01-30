05e94c5f6f0bbd4a542673f834e8e794fcdea2f8
=Vanessa <=a23vanessabg@iessanclemente.net>
2024-01-30 13:01:57 +0100

    Martes_30

--- src/test/java/dev/selenium/ChromeTest.java
+++ src/test/java/dev/selenium/ChromeTest.java
@@ -3,6 +3,8 @@ package dev.selenium;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.io.File;
+
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -93,15 +95,42 @@ public class ChromeTest{
       Thread.sleep(2000);
       
       assertTrue(checked.isSelected());//introducir en un sout si deseamos respuesta, si es true no devuelve nada*/
-
-      WebElement desabledInput = driver.findElement(By.name("my-disabled"));
-      desabledInput.sendKeys("Lunes por la mañana");
+      try {
+         WebElement desabledInput = driver.findElement(By.name("my-disabled"));
+         desabledInput.sendKeys("Lunes por la mañana");
       
+         Thread.sleep(2000);
+      } catch (Exception e) {
+         
+      }
+      driver.quit();
+
+   }
+   @Test  
+   public void elementUploadForms() throws InterruptedException{//carga de archivo
+      ChromeOptions options = new ChromeOptions();
+      driver = new ChromeDriver(options);
+      String archivo = "Loro.jpeg";
+      driver.get("https://the-internet.herokuapp.com/upload");
+
+      File uploadFile = new File("src/Files/"+archivo);/*absoluta: /home/sanclemente.local/a23vanessabg/Escritorio/CD_UD3_Selenium_maven/src/Files/Loro.jpeg */
+      WebElement fileInput = driver.findElement(By.cssSelector("input[type=file]"));//pasar solo el tipo que queremos
+      fileInput.sendKeys(uploadFile.getAbsolutePath());//ruta absoluta NO porque solo funcionaría en este ordenador
+      
+      Thread.sleep(1000);
+
+      driver.findElement(By.id("file-submit")).click();//forma abreviada de hacer click en lugar de crear el elemento
+      //buttonElement.click();
       Thread.sleep(2000);
 
-      
-      driver.quit();
-   }
+      WebElement fileName = driver.findElement(By.id("uploaded-files"));
+      assertEquals(archivo, fileName.getText());//comparará el esperado "Loro.jpeg" con el nombre que devuelve la página como nombre de archivo
+      /*escribir "Loro.jpeg" se denomina LITERAL no es aconsejable */
+      Thread.sleep(2000);
 
+      driver.quit();
+
+
+   }
 
 }

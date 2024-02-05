1e07f93fa9c751d4a5c296a8019356013ecd8822
=Vanessa <=a23vanessabg@iessanclemente.net>
2024-02-05 12:15:54 +0100

    lunes 5

--- src/test/java/dev/selenium/Proyecto_Test.java
+++ src/test/java/dev/selenium/Proyecto_Test.java
@@ -15,7 +15,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.Select;
 
-public class ChromeTest{
+public class Proyecto_Test{
 
  static ChromeDriver driver;//abre ventana de chrome para probar app o pagweb
 
@@ -24,13 +24,13 @@ public class ChromeTest{
    ChromeOptions options = new ChromeOptions();
    options.addArguments("start-maximized");
    driver = new ChromeDriver(options);
-   driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+   driver.get("https://www.maquillalia.com/");
  }
 
  @AfterAll//se coloca al final del programa por lógica
  public static void end(){
    try {
-      Thread.sleep(3000);
+      Thread.sleep(5000);
    } catch (Exception e) {
       e.printStackTrace();
    }
@@ -44,21 +44,10 @@ public class ChromeTest{
     WebElement textInput = driver.findElement(By.id("my-text-id"));//buscar un id por su nombre
     textInput.sendKeys("vanessa");//para probarlo escribimos algo y debemos incluir después el thread
 
-    /*try {
-      Thread.sleep(3000);
-   } catch (InterruptedException e) {
-      
-      e.printStackTrace();
-   }*/
 
    WebElement button = driver.findElement(By.className("btn-outline-primary"));//en caso de ser clase, copiar únicamente el nombre central(caso contrario: excepción)
    button.click();
-   /*try {
-      Thread.sleep(5000);
-   } catch (InterruptedException e) {
-      
-      e.printStackTrace();
-   }*/
+   
 
    WebElement mensaje = driver.findElement(By.id("message"));
    System.out.println(mensaje.getText());
@@ -68,10 +57,10 @@ public class ChromeTest{
  }
 
    @Test  
-   public void elementsForms() throws InterruptedException{
+   public void CPF1() throws InterruptedException{
       ChromeOptions options = new ChromeOptions();
       driver = new ChromeDriver(options);
-      driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+      driver.get("https://www.maquillalia.com/");
 
       /*WebElement checked = driver.findElement(By.id("my-check-1"));
       checked.click();
@@ -79,36 +68,30 @@ public class ChromeTest{
       
       assertTrue(checked.isSelected());//introducir en un sout si deseamos respuesta, si es true no devuelve nada*/
 
-      WebElement desabledInput = driver.findElement(By.name("my-disabled"));
-      String textTest = "Test";
-      desabledInput.sendKeys("Lunes por la mañana");
-      assertEquals(textTest, desabledInput.getText());
-      Thread.sleep(2000);
 
+      WebElement search = driver.findElement(By.name("buscar"));
+      search.sendKeys("Eyeliner");
+      search.sendKeys(Keys.ENTER);
       
-      //driver.quit();
+      Thread.sleep(5000);
+
    }
 
    @Test  
-   public void elementDisableForms() throws InterruptedException{
+   public void CPF2() throws InterruptedException{
       ChromeOptions options = new ChromeOptions();
       driver = new ChromeDriver(options);
-      driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+      driver.get("https://www.maquillalia.com/");
 
-      /*WebElement checked = driver.findElement(By.id("my-check-1"));
-      checked.click();
-      Thread.sleep(2000);
+      WebElement search = driver.findElement(By.name("buscar"));
+      search.sendKeys("Corazona");
+      search.sendKeys(Keys.ENTER);
+
+      WebElement categoria = driver.findElement(By.className("tab-item"));
+      categoria.click();
+     
+    Thread.sleep(3000);
       
-      assertTrue(checked.isSelected());//introducir en un sout si deseamos respuesta, si es true no devuelve nada*/
-      try {
-         WebElement desabledInput = driver.findElement(By.name("my-disabled"));
-         desabledInput.sendKeys("Lunes por la mañana");
-      
-         Thread.sleep(2000);
-      } catch (Exception e) {
-         
-      }
-      //driver.quit();
 
    }
    @Test  
